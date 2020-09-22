dbe480f88d1a09bd47a72abe7cdc913c89c89e31
singhshivanshu <shivanshusingh12@gmail.com>
2020-09-22 18:58:12 +0530

    first commit

---
+++ src/component/ChatBox/index.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import "./style.css";
+import { ReactComponent as Robot } from "../../pics/Robot.svg";
+import { ReactComponent as Hand } from "../../pics/Hand.svg";
+import { ReactComponent as Message } from "../../pics/Message.svg";
+import Button from "../Button";
+
+function ChatBox() {
+  const [show, setShow] = useState(false)
+
+  return (
+    <>
+    <div className="box" style={{display: show ? "flex" : "none"}}>
+      <div className="second-half"></div>
+      <div className="first-half">
+        <div className="welcome-message">
+          <div className="line-1">IRIS</div>
+          <div className="line-2">
+            <p>Hello</p> <Hand className="hand" />
+          </div>
+          <div className="line-3">
+            I am Iris, a Virtual Assistant How may I help you?
+          </div>
+        </div>
+      </div>
+      <div className="middle-block">
+        <h4 className="heading">Frequently Asked Questions ?</h4>
+        <div className="chat-container c-1">
+          <span className="note-point"></span>
+          <span className="message-container">
+            Can I redeem my FB before the original term?
+          </span>
+        </div>
+        <div className="chat-container c-2">
+          <span className="note-point"></span>
+          <span className="message-container">
+            How do I pay my Credit card bill?
+          </span>
+        </div>
+        <div className="chat-container c-3">
+          <span className="note-point"></span>
+          <span className="message-container">
+            How can I get my Account Statement?
+          </span>
+        </div>
+        <div className="chat-container c-4">
+          <span className="note-point"></span>
+          <span className="message-container">
+            What is the tenure of Fixed Deposit?
+          </span>
+        </div>
+        <div className="chat-button">
+          <span className="button-message">
+            <Message className="message"/>
+            <p>Start a New Conversation</p>
+          </span>
+        </div>
+      </div>
+      <div className="robot-container">
+        <Robot className="robot" />
+      </div>
+    </div>
+    <Button onClick={() => setShow(!show)}/>
+    
+    </>
+  );
+}
+
+export default ChatBox;
